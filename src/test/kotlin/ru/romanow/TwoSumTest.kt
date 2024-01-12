abb55f16b20e0e28b5d30b71e9b6f391856e3ac6
Romanow <romanowalex@mail.ru>
2024-01-12 23:54:09 +0300

    fix run

--- src/test/kotlin/ru/romanow/TwoSumTest.kt
+++ src/test/kotlin/ru/romanow/TwoSumTest.kt
@@ -25,7 +25,7 @@ class TwoSumTest {
         override fun provideArguments(context: ExtensionContext): Stream<Arguments> =
             Stream.of(
                 Arguments.of(intArrayOf(2, 7, 11, 15), 9, intArrayOf(0, 1)),
-                Arguments.of(intArrayOf(3, 2, 4), 2, intArrayOf(1, 2)),
+                Arguments.of(intArrayOf(3, 2, 4), 6, intArrayOf(1, 2)),
                 Arguments.of(intArrayOf(3, 3), 6, intArrayOf(0, 1)),
             )
     }
