f00c78c7c9360d158056e118930f38163a0e1f83
romanow <romanowalex@mail.ru>
2024-01-18 01:29:33 +0300

    fix description

--- src/main/kotlin/ru/romanow/IntegerToRoman.kt
+++ src/main/kotlin/ru/romanow/IntegerToRoman.kt
@@ -29,7 +29,32 @@ package ru.romanow
  * [https://leetcode.com/problems/integer-to-roman/](https://leetcode.com/problems/integer-to-roman/)
  */
 class IntegerToRoman {
+    private val map =
+        mapOf(
+            1000 to "M",
+            900 to "CM",
+            500 to "D",
+            400 to "CD",
+            100 to "C",
+            90 to "XC",
+            50 to "L",
+            40 to "XL",
+            10 to "X",
+            9 to "IX",
+            5 to "V",
+            4 to "IV",
+            1 to "I"
+        )
+
     fun intToRoman(num: Int): String {
-        return ""
+        var result = ""
+        var number = num
+        for ((value, symbol) in map) {
+            while (number >= value) {
+                result += symbol
+                number -= value
+            }
+        }
+        return result
     }
 }
